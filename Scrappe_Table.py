65e1e553ed0bd55c5aa108036e213bcd15103cc8
danzech <dani.zech@gmail.com>
2024-02-23 19:10:07 +0100

    table_scrapper

--- Scrappe_Table.py
+++ Scrappe_Table.py
@@ -1,50 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from urllib.parse import urlparse, parse_qs
 
 #/html/body/table[2]/tbody/tr[1]/td[4]/table/tbody/tr[2]/td/form/table/thead
 
 # webpage_url da página
-url_base = "https://juris.bundesgerichtshof.de"
-document_path = "/cgi-bin/rechtsprechung/list.py?Gericht=bgh&Art=en&Datum=Aktuell&Sort=12288"
-webpage_url = f"{url_base}{document_path}"
+url_base = "https://juris.bundesgerichtshof.de/"
+document_path = "cgi-bin/rechtsprechung/list.py?Gericht=bgh&Art=en&Datum=Aktuell&Seite="
+pages = range(0,43) # 
 
-response = requests.get(webpage_url)
+
+def unique_nr_extract(href):
+    query_string = urlparse(href).query # Retorna a query string da URL
+    parametros = parse_qs(query_string)     # Retorna um dicionário com os parâmetros da query string
+    numero_nr = parametros.get('nr', [None])[0]  # Retorna None se 'nr' não existir
+    return numero_nr
 
 decisions = []
 
-if response.status_code == 200:
-    html_content = response.text
-    soup = BeautifulSoup(html_content, "html.parser")
-  
-    # Find all rows in the table
-    rows = soup.find("tbody").find_all("tr")
+for page in pages:
+    webpage_url = f"{url_base}{document_path}{page}"
+    response = requests.get(webpage_url)
+    
+    if response.status_code == 200:
+        html_content = response.text
+        soup = BeautifulSoup(html_content, "html.parser")
 
-    # Iterate over the rows and extract the text (exclude the thead and tfoot rows)  
+        columms = soup.find("tbody").find_all("tr")
 
-    for row in rows:
-        # Extract the text from each cell
-        cells = row.find_all("td")
+        for columm in columms:
+            cells = columm.find_all("td")
+            if len(cells) == 5:
+                
+                all_links_aktenzeichen = cells[3].find_all("a")
+                print(len(all_links_aktenzeichen))
+                link_to_parse = all_links_aktenzeichen[1] if all_links_aktenzeichen else None
+                link = (link_to_parse['href']) if link_to_parse else None
+                unique_nr = unique_nr_extract(link) if link else None
 
-        if len(cells) == 5: # garantir 5 colunas (senado, data da decisão, data de publicação, número do processo, adtional info)
-            decision = {
-                "Senat": cells[0].text.strip(),
-                "Entscheidungsdatum": cells[1].text.strip(),
-                "Einspieldatum": cells[2].text.strip(),
-                "Aktenzeichen": cells[3].text.strip(),
-                "Zusatzinformationen": cells[4].text.strip()
-            }
+                decision = {
+                    "Senat": cells[0].text.strip(),
+                    "Entscheidungsdatum": cells[1].text.strip(),
+                    "Einspieldatum": cells[2].text.strip(),
+                    "Aktenzeichen": cells[3].text.strip(),
+                    "Link_Aktenzeichen": f"{url_base}{link}",
+                    "Zusatzinformationen": cells[4].text.strip(),
+                    "Unique_nr": unique_nr
+                   
+                }
 
-            decisions.append(decision)
+                decisions.append(decision)
+                print(unique_nr)
 
 df = pd.DataFrame(decisions)
 
 # print(df)
 
+
 # Save the dataframe to a csv file
 local_path= "bundesgerichtshof_local/data/decisions_table.csv"
 working_path = "//mnt/c/Users/daniz/OneDrive/Documentos/iur.crowd/decisions_table.csv"
-df.to_csv(local_path, index=False, encoding='utf-8')  # Save without index and with utf-8 encoding
-df.to_csv(working_path, index=False, encoding='utf-8')  # Save without index and with utf-8 encoding
+df.to_csv(local_path, index=False, encoding='utf-8', sep=';')  # Save without index and with utf-8 encoding using semicolon (;) as separator
+df.to_csv(working_path, index=False, encoding='utf-8', sep=';')  # Save without index and with utf-8 encoding using semicolon (;) as separator
 
-
+print(f"File saved to {local_path}")
